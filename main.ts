ab42f5a14f714f8f1808a3d5afcc34bd7f51f777
Iulian Onofrei <5748627+revolter@users.noreply.github.com>
2024-02-18 14:21:33 +0200

    Project: Replaced the sample plugin with Hanayama Huzzles tracker
    https://github.com/revolter/obsidian-hanayama-huzzles-tracker-plugin

--- main.ts
+++ main.ts
@@ -1,134 +1,263 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import dedent from 'dedent';
+import escapeStringRegExp from 'escape-string-regexp';
+import { Image, PhrasingContent, Table, TableCell, TableRow, Text } from 'mdast';
+import { toString } from 'mdast-util-to-string';
+import { Notice, Plugin, requestUrl } from 'obsidian';
+import { remark } from 'remark';
+import remarkGFM from 'remark-gfm';
+import { Root } from 'remark-gfm/lib';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+class HanayamaHuzzle {
+	constructor(
+		public level: string,
+		public index: string,
+		public name: string,
+		public imageLinks: string[],
+		public status = ''
+	) {}
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class HanayamaHuzzlesTrackerPlugin extends Plugin {
+	static #startMarker = '<!-- Hanayama Huzzles start -->';
+	static #endMarker = '<!-- Hanayama Huzzles end -->';
+	static #headers = ['Level', 'Index', 'Name', 'Picture', 'Status'];
+	static #scrapeUrls = [
+		'https://hanayama-toys.com/product-category/puzzles/huzzle/level-1-fun',
+		'https://hanayama-toys.com/product-category/puzzles/huzzle/level-2-easy',
+		'https://hanayama-toys.com/product-category/puzzles/huzzle/level-3-normal',
+		'https://hanayama-toys.com/product-category/puzzles/huzzle/level-4-hard',
+		'https://hanayama-toys.com/product-category/puzzles/huzzle/level-5-expert',
+		'https://hanayama-toys.com/product-category/puzzles/huzzle/level-6-grand-master',
+		'https://hanayama-toys.com/product-category/puzzles/huzzle/chess-puzzle'
+	]
 
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+			id: 'update-list',
+			name: 'Update list',
+			editorCallback: (editor, _view) => {
+				const content = editor.getValue();
+
+				new Notice('List update started');
+				const startDate = new Date();
+
+				this.#updatedListInContent(content).then( newContent => {
+					editor.setValue(newContent);
+
+					const endDate = new Date();
+					const seconds = (endDate.getTime() - startDate.getTime()) / 1000;
+					new Notice(`List update finished in ${seconds} seconds`);
+				});
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
+	}
+
+	onunload() {}
+
+	async #updatedListInContent(content: string): Promise<string> {
+		const escapedStartMarker = escapeStringRegExp(HanayamaHuzzlesTrackerPlugin.#startMarker);
+		const escapedEndMarker = escapeStringRegExp(HanayamaHuzzlesTrackerPlugin.#endMarker);
+
+		const regex = new RegExp(`${escapedStartMarker}(?<markdownList>.*?)${escapedEndMarker}`, 's');
+		const match = content.match(regex);
+
+		if (match != null && match.groups != null) {
+			const markdownList = match.groups.markdownList;
+			const currentHuzzles = this.#markdownTableToHuzzles(markdownList);
+			const updatedMarkdownList = await this.#updatedHuzzles(currentHuzzles);
+
+			return content.replace(regex, updatedMarkdownList);
+		} else {
+			const updatedMarkdownList = await this.#updatedHuzzles([]);
+
+			return dedent`
+				${content}
+
+				${updatedMarkdownList}
+			`;
+		}
+	}
+
+	async #updatedHuzzles(currentHuzzles: HanayamaHuzzle[]): Promise<string> {
+		const indexedCurrentHuzzles = currentHuzzles.slice(1).reduce((map, huzzle) => {
+			map[huzzle.name] = huzzle;
+
+			return map;
+		}, {} as {[key: string]: HanayamaHuzzle});
+
+		const huzzles = (await this.#scrapeAllHuzzles()).flat();
+		huzzles.forEach( huzzle => {
+			const indexedCurrentHuzzle = indexedCurrentHuzzles[huzzle.name];
+
+			if (indexedCurrentHuzzle !== undefined) {
+				huzzle.status = indexedCurrentHuzzle.status;
+
+				delete indexedCurrentHuzzles[huzzle.name];
 			}
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+
+		const withdrawnHuzzles = Object.keys(indexedCurrentHuzzles).map(key => indexedCurrentHuzzles[key]);
+		const withdrawnModifiedHuzzles = withdrawnHuzzles.filter(huzzle => huzzle.status !== '');
+
+		const updatedList = this.#huzzlesToMarkdownTableString(HanayamaHuzzlesTrackerPlugin.#headers, [...huzzles, ...withdrawnModifiedHuzzles]);
+
+		return dedent`
+			${HanayamaHuzzlesTrackerPlugin.#startMarker}
+
+			${updatedList}
+
+			${HanayamaHuzzlesTrackerPlugin.#endMarker}
+		`;
+	}
+
+	async #scrapeAllHuzzles(): Promise<HanayamaHuzzle[][]> {
+		return await Promise.all(HanayamaHuzzlesTrackerPlugin.#scrapeUrls.flatMap(async url => {
+			return await this.#scrapeHuzzles(url);
+		}));
+	}
+
+	async #scrapeHuzzles(url: string): Promise<HanayamaHuzzle[]> {
+		const response = await requestUrl(url);
+		const content = new DOMParser().parseFromString(response.text, 'text/html');
+		const products = Array.from(content.querySelectorAll('#main > .products > .product'));
+		const metadataRegex = new RegExp(/\w+[ ](?<level>\d+)-(?<index>\d+)[ ](?<name>.+)/); // https://regex101.com/r/1vGzHd/2
+
+		return products.flatMap(product => {
+			const title = product.querySelector('.product-info > .product-title > a')?.textContent || '';
+			const titleMatch = title.match(metadataRegex);
+
+			let level: string
+			let index: string
+			let name: string
+
+			if (titleMatch != null && titleMatch.groups != null) {
+				level = titleMatch.groups.level;
+				index = titleMatch.groups.index;
+				name = titleMatch.groups.name;
+			} else {
+				level = 'N/A';
+				index = 'N/A';
+				name = title;
+			}
+
+			const images = product.querySelectorAll('.product-thumb > a > img');
+			const imageLinks = Array.from(images, image => (image as HTMLImageElement).src);
+
+			return new HanayamaHuzzle(level, index, name, imageLinks);
+		});
+	}
+
+	#huzzlesToMarkdownTableString(headers: string[], huzzles: HanayamaHuzzle[]): string {
+		const headerRow = this.#tableRowNode(headers.map(header => this.#textTableCellNode(header)));
+		const huzzleRows = huzzles.map(huzzle =>
+			this.#tableRowNode([
+				this.#textTableCellNode(huzzle.level),
+				this.#textTableCellNode(huzzle.index),
+				this.#textTableCellNode(huzzle.name),
+				this.#tableCellNode(
+					this.#interleave(
+						huzzle.imageLinks.map(imageLink => this.#imageNode(imageLink, 100)),
+						this.#textNode(' ')
+					)
+				),
+				this.#textTableCellNode(huzzle.status)
+			])
+		);
+		const tableRows = [
+			...[headerRow],
+			...huzzleRows
+		]
+		const table: Table = {
+			type: 'table',
+			children: tableRows as never // https://stackoverflow.com/a/47219058/865175
+		};
+		const root: Root = {
+			type: 'root',
+			children: [table]
+		};
+
+		return remark()
+			.use(remarkGFM)
+			.stringify(root)
+			.replace(/\n$/, '');
+	}
+
+	#tableRowNode(children: TableCell[]): TableRow {
+		return {
+			type: 'tableRow',
+			children: children
+		}
+	}
+
+	#textTableCellNode(text: string): TableCell {
+		return this.#tableCellNode([this.#textNode(text)]);
+	}
+
+	#tableCellNode(children: PhrasingContent[]): TableCell {
+		return {
+			type: 'tableCell',
+			children: children
+		}
+	}
+
+	#textNode(text: string): Text {
+		return {
+			type: 'text',
+			value: text
+		}
+	}
+
+	#imageNode(url: string, size: number): Image {
+		return {
+			type: 'image',
+			alt: `|${size}`,
+			url: url
+		}
+	}
+
+	#interleave<Array extends PhrasingContent, Separator extends PhrasingContent>(array: Array[], separator: Separator) {
+		return array.flatMap(element => [separator, element]).slice(1);
+	}
+
+	#markdownTableToHuzzles(markdownTableString: string): HanayamaHuzzle[] {
+		const ast = remark()
+			.use(remarkGFM)
+			.parse(markdownTableString);
+		const table = ast.children.find(node => node.type === 'table') as Table;
+		const arrayOfArrays = table.children.map(row =>
+			row.children.map(cell =>
+				cell.children.map(child => {
+					switch (child.type) {
+						case 'image': return `![${child.alt}](${child.url})`
+						default: return toString(child)
 					}
+				}).join('')
+			)
+		);
+		const imageLinksRegex = new RegExp(/!\[[^\]]+\]\((?<link>[^)]+)(?=\))/g); // https://regex101.com/r/YlCOgc/2
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+		return arrayOfArrays.flatMap(array => {
+			if (array.length < 5) {
+				return [];
 			}
+
+			const level = array[0];
+			const index = array[1];
+			const name = array[2];
+
+			const images = array[3];
+			const imageLinkMatches = images.matchAll(imageLinksRegex);
+			const imageLinks = Array.from(imageLinkMatches).flatMap(match => {
+				if (match == null || match.groups == null) {
+					return [];
+				}
+
+				return match.groups.link;
+			});
+
+			const status = array[4];
+
+			return new HanayamaHuzzle(level, index, name, imageLinks, status);
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
