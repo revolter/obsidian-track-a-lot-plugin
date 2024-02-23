ee8658e8417a42e6b3a0a660374a51daabc4315a
Iulian Onofrei <5748627+revolter@users.noreply.github.com>
2024-02-23 23:29:04 +0200

    Refactor: Moved some scaffolding logic to `WebsiteScraper`

--- src/scraping/WebsiteScraper.ts
+++ src/scraping/WebsiteScraper.ts
@@ -3,14 +3,17 @@ import { WebpageDownloader } from './WebpageDownloader';
 export class WebsiteScraper {
 	constructor(private urls: string[]) {}
 
-	async scrape(): Promise<Document[]> {
-		return Promise.all(
+	async scrape(parseWebpage: (webpage: Document) => Element[]): Promise<Element[]> {
+		const elements = await Promise.all(
 			this.urls
 			.map(async url => {
 				const downloader = new WebpageDownloader(url);
+				const webpage = await downloader.download();
 
-				return await downloader.download();
+				return parseWebpage(webpage);
 			})
 		);
+
+		return elements.flat();
 	}
 }
