6dd3e0ff1d241ecb8cb9eb1e9eab04d781793ac6
Iulian Onofrei <5748627+revolter@users.noreply.github.com>
2024-02-24 11:07:53 +0200

    Refactor: Removed superfluous method

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { Plugin } from 'obsidian';
 import { MarkdownTableConverter } from './markdown/MarkdownTableConverter';
 import { MarkdownTableFactory } from './markdown/MarkdownTableFactory';
 import { PluginUpdateCommandFactory } from './plugin/PluginUpdateCommandFactory';
-import { Recipe } from './recipes/Recipe';
 import { HanayamaHuzzlesRecipe } from './recipes/hanayama_huzzles/HanayamaHuzzlesRecipe';
 import { IQPuzzlesRecipe } from './recipes/iq_puzzles/IQPuzzlesRecipe';
 import { RecipesSettingsTab } from './settings/RecipesSettingsTab';
@@ -20,29 +19,28 @@ export default class TrackALotPlugin extends Plugin {
 		const markdownTableFactory = new MarkdownTableFactory();
 		const markdownTableConverter = new MarkdownTableConverter();
 		const trackablesUpdater = new TrackablesUpdater();
+		const commandFactory = new PluginUpdateCommandFactory();
 
 		if (settingsManager.settings.hanayamaHuzzles.isActive) {
-			this.#addCommand(HanayamaHuzzlesRecipe.NAME, new HanayamaHuzzlesRecipe(
+			const command = commandFactory.command(HanayamaHuzzlesRecipe.NAME, new HanayamaHuzzlesRecipe(
 				markdownTableFactory,
 				markdownTableConverter,
 				trackablesUpdater
 			));
+
+			this.addCommand(command);
 		}
 
 		if (settingsManager.settings.iqPuzzles.isActive) {
-			this.#addCommand(IQPuzzlesRecipe.NAME, new IQPuzzlesRecipe(
+			const command = commandFactory.command(IQPuzzlesRecipe.NAME, new IQPuzzlesRecipe(
 				markdownTableFactory,
 				markdownTableConverter,
 				trackablesUpdater
 			));
+
+			this.addCommand(command);
 		}
 	}
 
 	onunload() {}
-
-	#addCommand(name: string, recipe: Recipe) {
-		const factory = new PluginUpdateCommandFactory();
-
-		this.addCommand(factory.command(name, recipe));
-	}
 }
