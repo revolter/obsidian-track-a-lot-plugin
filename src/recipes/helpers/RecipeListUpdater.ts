5241823a721da233f67fac3e21ffe5104fa45251
Iulian Onofrei <5748627+revolter@users.noreply.github.com>
2025-06-19 23:11:00 +0300

    Refactor: Made generic type name clearer

--- src/recipes/helpers/RecipeListUpdater.ts
+++ src/recipes/helpers/RecipeListUpdater.ts
@@ -2,7 +2,7 @@ import { Trackable } from 'src/tracking/Trackable';
 import { TrackablesUpdater } from 'src/tracking/TrackablesUpdater';
 import { RecipeMarkdownListUpdater } from './RecipeMarkdownListUpdater';
 
-export class RecipeListUpdater<T extends Trackable> {
+export class RecipeListUpdater<RecipeItem extends Trackable> {
 	constructor(
 		private headers: readonly string[],
 		private recipeMarkdownListUpdater: RecipeMarkdownListUpdater,
@@ -11,9 +11,9 @@ export class RecipeListUpdater<T extends Trackable> {
 
 	async update(
 		content: string,
-		markdownTableStringToTrackables: (markdownTableString: string) => T[],
-		scrapeTrackables: () => Promise<T[]>,
-		trackablesToMarkdownTableString: (headers: readonly string[], trackables: T[]) => string
+		markdownTableStringToTrackables: (markdownTableString: string) => RecipeItem[],
+		scrapeTrackables: () => Promise<RecipeItem[]>,
+		trackablesToMarkdownTableString: (headers: readonly string[], trackables: RecipeItem[]) => string
 	): Promise<string> {
 		return this.recipeMarkdownListUpdater.update(content, async markdownList => {
 			const currentTrackables = markdownList != null ? markdownTableStringToTrackables(markdownList) : [];
